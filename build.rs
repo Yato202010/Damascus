bb37ece898ce405bf26a3e40334c2c227a9d3759
Yato202010 <61349049+Yato202010@users.noreply.github.com>
2024-04-14 12:12:40 +0200

    build(dep+build): fix gitmodule + update build script

--- build.rs
+++ build.rs
@@ -1,22 +1,27 @@
+use std::{ffi::OsStr, fmt::Debug, path::PathBuf};
+
 fn main() {
     #[cfg(feature = "fuse-overlayfs-vendored")]
     {
         let d = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("vendor/fuse-overlayfs/");
-        use std::process::Command;
         if !d.exists() {
             panic!("fuse-overlayfs submodule is not present fuse-overlayfs-vendored cannot be used")
         }
-        Command::new(d.join("autogen.sh"))
-            .current_dir(&d)
-            .spawn()
-            .expect("Unable to Compile fuse-overlayfs");
-        Command::new(d.join("configure"))
-            .current_dir(&d)
-            .spawn()
-            .expect("Unable to Compile fuse-overlayfs");
-        Command::new("make")
-            .current_dir(&d)
-            .spawn()
-            .expect("Unable to Compile fuse-overlayfs");
+        let err = "Unable to Compile fuse-overlayfs";
+        let mut op = "autogen.sh";
+        run(err, d.join(op), &d);
+        op = "configure";
+        run(err, d.join(op), &d);
+        op = "make";
+        run(err, op, &d)
     }
+}
+
+#[inline]
+fn run<S: AsRef<OsStr> + Debug>(err: &str, op: S, dir: &PathBuf) {
+    use std::process::Command;
+    Command::new(&op)
+        .current_dir(dir)
+        .spawn()
+        .unwrap_or_else(|_| panic!("{:?}: {:?} failed", err, op));
 }
