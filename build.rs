bcdc8e16685d822f1cb1639a648d9fd5b71947c2
Yato202010 <61349049+Yato202010@users.noreply.github.com>
2024-09-06 16:28:22 +0200

    feat(compile time): reduce compile time when vendoring

--- build.rs
+++ build.rs
@@ -1,27 +1,32 @@
-use std::{ffi::OsStr, fmt::Debug, path::PathBuf};
+use std::{
+    ffi::OsStr,
+    fmt::Debug,
+    fs::create_dir_all,
+    path::{self, Path, PathBuf},
+};
 
 fn main() {
+    println!("cargo:rerun-if-changed=vendor");
+
     #[cfg(feature = "fuse-overlayfs-vendored")]
     {
+        // cache executable by storing it's hash in the target directory
         let d = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("vendor/fuse-overlayfs/");
         if !d.exists() {
             panic!("fuse-overlayfs submodule is not present fuse-overlayfs-vendored cannot be used")
         }
         let err = "Unable to Compile fuse-overlayfs";
-        let mut op = "autogen.sh";
-        run(err, d.join(op), &d);
-        op = "configure";
-        run(err, d.join(op), &d);
-        op = "make";
-        run(err, op, &d)
-    }
-    #[cfg(feature = "cicpoffs-vendored")]
-    {
-        let d = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("vendor/cicpoffs/");
-        if !d.exists() {
-            panic!("cicpoffs submodule is not present cicpoffs-vendored cannot be used")
+        let executable = d.join("fuse-overlayfs");
+
+        if !is_cache_valid(&executable) {
+            let mut op = "autogen.sh";
+            run(err, d.join(op), &d);
+            op = "configure";
+            run(err, d.join(op), &d);
+            op = "make";
+            run(err, op, &d);
+            cache(&executable);
         }
-        run("Unable to Compile cicpoffs", "make", &d)
     }
 }
 
@@ -32,5 +37,40 @@ fn run<S: AsRef<OsStr> + Debug>(err: &str, op: S, dir: &PathBuf) {
     Command::new(&op)
         .current_dir(dir)
         .spawn()
-        .unwrap_or_else(|_| panic!("{:?}: {:?} failed", err, op));
+        .unwrap_or_else(|_| panic!("{:?}: {:?} failed", err, op))
+        .wait()
+        .unwrap();
+}
+
+const CACHE_DIR: &str = "target/.cache";
+
+#[inline]
+#[allow(dead_code)]
+fn is_cache_valid<S: AsRef<Path> + Debug>(path: S) -> bool {
+    let path = path.as_ref();
+    if !path.exists() {
+        return false;
+    }
+    let cache = Path::new(CACHE_DIR).join(path.file_name().unwrap());
+    if !cache.exists() {
+        return false;
+    }
+    let old_hash = std::fs::read(cache).unwrap();
+    let buf = std::fs::read(path).unwrap();
+    let new_hash = md5::compute(&buf).0;
+    old_hash == new_hash
+}
+
+#[inline]
+#[allow(dead_code)]
+fn cache<S: AsRef<Path> + Debug>(path: S) {
+    let path = path.as_ref();
+    let buf = std::fs::read(path).unwrap();
+    let hash = md5::compute(&buf).0;
+    if !PathBuf::from(CACHE_DIR).exists() {
+        create_dir_all(CACHE_DIR).unwrap();
+    }
+
+    let cache = Path::new(CACHE_DIR).join(path.file_name().unwrap());
+    std::fs::write(cache, hash).unwrap();
 }
--- src/common/utils/partition.rs
+++ src/common/utils/partition.rs
@@ -1,4 +1,8 @@
-use std::path::Path;
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 /// Representation of a partition unique identifier
@@ -16,6 +20,7 @@ impl<P> From<P> for PartitionID
 where
     P: AsRef<Path>,
 {
+    /// Create a new PartitionID from a path
     #[inline]
     fn from(path: P) -> PartitionID {
         let path = path.as_ref();
@@ -23,65 +28,65 @@ where
             return PartitionID::Invalid;
         }
 
-        cfg_if::cfg_if! {
-            if #[cfg(target_family="unix")] {
-                use std::os::unix::fs::MetadataExt;
-                PartitionID::Id(std::fs::metadata(path)
+        #[cfg(target_family = "unix")]
+        {
+            use std::os::unix::fs::MetadataExt;
+            PartitionID::Id(
+                std::fs::metadata(path)
                     .expect("unable to get metadata")
-                    .dev())
-            } else if #[cfg(target_os = "windows")] {
-                unsafe {
-                    extern crate windows as win;
-                    use std::ptr;
-                    use win::{
-                        Win32::{
-                            Storage::FileSystem::{
-                                GetVolumeInformationByHandleW,
-                                CreateFileA,
-                                FILE_ATTRIBUTE_NORMAL,
-                                OPEN_EXISTING,
-                                FILE_SHARE_READ,
-                                FILE_GENERIC_READ,
-                                FILE_GENERIC_WRITE
-                            },
-                            Foundation::HANDLE
+                    .dev(),
+            )
+        }
+        #[cfg(target_os = "windows")]
+        {
+            unsafe {
+                extern crate windows as win;
+                use std::ptr;
+                use win::{
+                    core::PCSTR,
+                    Win32::{
+                        Foundation::HANDLE,
+                        Storage::FileSystem::{
+                            CreateFileA, GetVolumeInformationByHandleW, FILE_ATTRIBUTE_NORMAL,
+                            FILE_GENERIC_READ, FILE_GENERIC_WRITE, FILE_SHARE_READ, OPEN_EXISTING,
                         },
-                        core::PCSTR
-                    };
+                    },
+                };
 
-                    let path_str = path.to_str().unwrap().as_bytes();
-                    let path_wstr = PCSTR::from_raw(path_str.as_ptr());
+                let path_str = path.to_str().unwrap().as_bytes();
+                let path_wstr = PCSTR::from_raw(path_str.as_ptr());
 
-                    let handle = CreateFileA(
-                        path_wstr,
-                        FILE_GENERIC_READ.0 | FILE_GENERIC_WRITE.0,
-                        FILE_SHARE_READ,
-                        None,
-                        OPEN_EXISTING,
-                        FILE_ATTRIBUTE_NORMAL,
-                        HANDLE(0),
-                    ).unwrap();
-                    let lpvolumeserialnumber = ptr::null_mut();
-                        GetVolumeInformationByHandleW(
-                            handle,
-                            None,
-                            Some(lpvolumeserialnumber),
-                            None,
-                            None,
-                            None
-                        ).unwrap();
-                    PartitionID::Id(*lpvolumeserialnumber)
-                }
-
-                // TODO : move to safe alternative once into rust stable
-                //
-                // use std::os::windows::fs::MetadataExt;
-                // let dev = std::fs::metadata(path)
-                //     .expect("unable to get metadata")
-                //     .volume_serial_number()
-                //     .unwrap("unable to get volume serial number");
-                // Partition::Id(dev)
+                let handle = CreateFileA(
+                    path_wstr,
+                    FILE_GENERIC_READ.0 | FILE_GENERIC_WRITE.0,
+                    FILE_SHARE_READ,
+                    None,
+                    OPEN_EXISTING,
+                    FILE_ATTRIBUTE_NORMAL,
+                    HANDLE(0),
+                )
+                .unwrap();
+                let lpvolumeserialnumber = ptr::null_mut();
+                GetVolumeInformationByHandleW(
+                    handle,
+                    None,
+                    Some(lpvolumeserialnumber),
+                    None,
+                    None,
+                    None,
+                )
+                .unwrap();
+                PartitionID::Id(*lpvolumeserialnumber)
             }
+
+            // TODO : move to safe alternative once into rust stable
+            //
+            // use std::os::windows::fs::MetadataExt;
+            // let dev = std::fs::metadata(path)
+            //     .expect("unable to get metadata")
+            //     .volume_serial_number()
+            //     .unwrap("unable to get volume serial number");
+            // Partition::Id(dev)
         }
     }
 }
@@ -92,12 +97,15 @@ mod tests {
 
     #[test]
     fn from() {
-        cfg_if::cfg_if! {
-            if #[cfg(target_family="unix")] {
-                let _ = PartitionID::from("/tmp/");
-            } else if #[cfg(target_os = "windows")] {
-                let _ = PartitionID::from("C://User");
-            }
+        #[cfg(target_family = "unix")]
+        {
+            let id = PartitionID::from("/tmp/");
+            assert_ne!(id, PartitionID::Id(0));
+        }
+        #[cfg(target_os = "windows")]
+        {
+            let id = PartitionID::from("C://User");
+            assert_ne!(id, PartitionID::Id(0));
         }
     }
 }
